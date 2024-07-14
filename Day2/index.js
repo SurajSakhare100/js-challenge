a487ddcd60893162247300626adbe5bc404c1399
suraj sakhare <sakharesuraj10@gmail.com>
2024-07-14 20:58:47 +0530

    Day 2 Task Completed

---
+++ Day2/index.js
@@ -0,0 +1,63 @@
+// Assignment 1
+
+// add two num 
+
+let a= 234;
+let b= 345;
+
+console.log(a+b);
+
+// subtract two num 
+
+a= 234;
+b= 345;
+
+// multiply two num
+
+console.log(a-b);
+a= 234;
+b= 345;
+
+console.log(a*b);
+
+// divide two num
+
+a= 234;
+b= 345;
+
+console.log(a/b);
+
+
+// modulo of two num
+
+a= 234;
+b= 345;
+
+console.log(b%a);
+
+// Assignment 2
+
+let c=2;
+c+=2;
+console.log(c)
+c-=2;
+console.log(c)
+
+let d=90;
+let e=45;
+
+// Assignment 3 
+
+console.log(d<e);
+console.log(d>e);
+console.log(d>=e);
+console.log(d<=e);
+console.log(d==e);
+
+let isAdmin=false;
+let isUser=true;
+console.log(isAdmin  && isUser)
+console.log(isAdmin  || isUser)
+console.log(!isUser)
+
+console.log(d>e?"D is Big":"D is small")
