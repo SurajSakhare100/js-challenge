10ee669473f008afd316331fb4c4c7d0ce2a6d0b
suraj sakhare <sakharesuraj10@gmail.com>
2024-07-16 10:39:54 +0530

    Day 4 Task Completed

---
+++ Day 4/index.js
@@ -0,0 +1,76 @@
+// //Assignment 1
+
+// // task 1
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// //task 2
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i*5)
+// }
+
+// //Assignment 2
+
+// let sum=0,i=1;
+// while (i<=10) {
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum)
+
+// let num=10;
+// while (num>0) {
+//     console.log(num);
+//     num--;
+// }
+
+// //Assignment 3
+
+// let z=1;
+// do {
+//     console.log(z)
+//     z++;
+// } while (z<=5);
+
+// let value=1;
+// let a=0;
+// let b=1;
+// console.log(a);
+// do {
+//     console.log(b);
+//     let temp = b; 
+//     b = a + b; 
+//     a = temp;
+//     value++; 
+// } while (value < 10); 
+
+
+
+// // Assignment 4
+
+// for (let i = 1; i <= 5; i++) {
+//   let stars = '';
+//    for (let j = 0; j < i; j++) {
+//     stars += '* ';
+//    }
+//    console.log(stars);
+// }
+
+
+// // Assignment 5
+
+// for (let i = 1; i <=10; i++) {
+//    if(i==5){
+//     continue;
+//    }
+//    console.log(i)   
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(i)   ;
+//     if(i==7){
+//      break;
+//     }
+//  }
