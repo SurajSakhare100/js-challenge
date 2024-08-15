ccbf5cc63b8ff4c98de55cda28523c77d966ab1e
suraj sakhare <sakharesuraj10@gmail.com>
2024-08-15 19:41:00 +0530

    Day 28 Ecoomerce App Completed

---
+++ Day 28 Ecommerce App/products.js
@@ -0,0 +1,62 @@
+const products = [
+  {
+    name: "Stylish Headphones",
+    price: 7999,
+    description: "Enjoy your music with these high-quality headphones.",
+    imageUrl: "https://img.freepik.com/free-photo/levitating-music-headphones-display_23-2149817601.jpg",
+  },
+  {
+    name: "Smart Watch",
+    price: 19999,
+    description: "Track your activities with this stylish smart watch.",
+    imageUrl: "https://img.freepik.com/free-photo/rendering-smart-home-device_23-2151039302.jpg",
+  },
+  {
+    name: "Wireless Earbuds",
+    price: 4999,
+    description: "Compact and powerful wireless earbuds.",
+    imageUrl: "https://img.freepik.com/free-photo/black-wireless-earbud-with-charging-case_1268-14371.jpg",
+  },
+  {
+    name: "Laptop",
+    price: 74999,
+    description: "Powerful laptop with a sleek design for all your needs.",
+    imageUrl: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "Gaming Console",
+    price: 39999,
+    description: "Experience immersive gaming with this next-gen console.",
+    imageUrl: "https://img.freepik.com/premium-photo/closeup-gaming-console-with-vibrant-color-s_1148322-79557.jpg",
+  },
+  {
+    name: "Drone",
+    price: 59999,
+    description: "Capture stunning aerial footage with this high-performance drone.",
+    imageUrl: "https://img.freepik.com/free-photo/white-drone-hovering-bright-blue-sky_158595-3345.jpg",
+  },
+  {
+    name: "Speaker",
+    price: 69199,
+    description: "Enjoy high-quality sound anywhere with this portable speaker.",
+    imageUrl: "https://img.freepik.com/free-photo/3d-music-related-scene_23-2151125082.jpg",
+  },
+  {
+    name: "Smartphone",
+    price: 54999,
+    description: "Latest smartphone with top-notch features and performance.",
+    imageUrl: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "Smart TV",
+    price: 219999,
+    description: "Smart Tv with AR capabilities.",
+    imageUrl: "https://img.freepik.com/free-photo/television-houseplants-room-scene-generative-ai_188544-12145.jpg",
+  },
+  {
+    name: "Camera",
+    price: 64999,
+    description: "High-resolution camera for professional photography.",
+    imageUrl: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  }
+];
